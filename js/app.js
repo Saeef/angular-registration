9bc0e43c5f9b95e8f956461f78394da50196c1eb
Stefan Lissia <stefan.emailacct@gmail.com>
2016-03-26 21:56:36 +0000

    setting up angular app

---
+++ js/app.js
@@ -0,0 +1,16 @@
+(function() {
+
+	var myApp = angular.module('myApp',[]);
+
+	myApp.controller('AppController',['$scope', function($scope) {
+
+		$scope.message = "Welcome to my App";
+
+
+
+	}]);
+
+
+
+
+})();
